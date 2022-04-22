a12fe387e97e58e385064957562786eae64e22c0
sojod-talaat <sojodtalaat990@hotmail.com>
2022-04-23 00:39:20 +0300

    three

--- PHP/ViewExperience.php
+++ PHP/ViewExperience.php
@@ -37,5 +37,27 @@
 
 </nav>
 </header>
+<div class="div3">
+                <h1>All Experiance Information</h1>
+<br>
+                <h2 class="web">graphic design <sub>Gaza Goes Global/Frelansar </sub></h2>
+                <p class="from">from 2/2019 to 8/2020</p>
+                <p class="work">I offer all services related to graphic design Whether it is print or web I have extensive experience in the field of social media design and complete visual identity I edit and manipulate images with Adobe Photoshop I am also working on other programs, including Adobe illustrator - after effect - Adobe premiere
+                </p>
+
+                <h2 class="web free">Flutter <sub>Udemy/Freelancer</sub></h2>
+                <p class="from">from 5/2020 to 5/2021</p>
+                <p class="work">I am responsible and punctual when working, I like to listen to the client, so that I can clearly understand their needs and vision of the project, and maybe suggest some interesting ideas. My Skills : * Development of mobile applications through a framework of filters and using the language of dart circuits 
+                    Design and build multiple databases: - Firestore Cloud -MySql - Sqlite - Firebase Realtime - Hive 
+                     Use of maps such as google maps 
+                     Design user interfaces proportionally on all types of displays 
+                    Include multiple types of API
+                </p>
+                <h2 class="web free"> Uxui <sub>Udemy/Frelansar</sub></h2>
+                <p class="from">from 5/2020 to 5/2021</p>
+                <p class="work">I work according to a systematic system in several stages, starting with the analysis of the project, its audience and competitors, then dividing the data and formulating it into initial designs, then presenting it in its final form as "ready interfaces". You will get a file for designs, icons, and elements used in the design, in addition to your submission as a template to facilitate the programmer's work when programming the project.
+                </p>
+               
+                  </div>
 </body>
 </html>
